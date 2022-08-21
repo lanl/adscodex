ff14cbf0f288e5b35d468130f14ba95272aae4d2
Latchesar Ionkov <lucho@ionkov.net>
2022-08-21 10:13:29 -0600

    three new methods to match fields
    - simple: slight modification of the original method
    - find: use oligo.Find to find the end of the match so we can get
    better estimate where the next field starts
    - triecat: use the new Trie concatenation functionality and match the
    whole oligo instead of field-by-field

--- utils/rosetta/generr/main.go
+++ utils/rosetta/generr/main.go
@@ -26,7 +26,17 @@ var ierr = flag.Float64("ierr", 0.1, "insertion error per position (percent)")
 var derr = flag.Float64("derr", 0.1, "deletion error per position (percent)")
 var serr = flag.Float64("serr", 0.1, "substitution error per position (percent)")
 var fnum = flag.Int("f", 2, "fragment number")
+var method = flag.String("m", "triecat", "method of matching (old, simple, find, triecat)")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+var trieCat *utils.Trie
+
+var methods = map[string] func(ol, eol oligo.Oligo, olen []int, pools []*utils.Pool, olmap []map[string]int) {
+	"old": matchOld,
+	"simple": matchSimple,
+	"find": matchFind,
+	"triecat": matchTriecat,
+}
 
 func genRand(r *rand.Rand, olen int) (ret oligo.Oligo) {
 	ret = long.New(olen)
@@ -46,13 +56,21 @@ func main() {
 	flag.Parse()
 
 	if *onum == 0 {
-		fmt.Fprintf(os.Stderr, "Expecting number of sequences to generate\n")
+		fmt.Fprintf(os.Stderr, "Error: Expecting number of sequences to generate\n")
 		return
 	}
 
 	if flag.NArg() == 0 {
-		fmt.Fprintf(os.Stderr, "Expecting oligo file(s)\n")
+		fmt.Fprintf(os.Stderr, "Error: Expecting oligo file(s)\n")
 		return
+	}
+
+	mm := matchTriecat
+	if mmt, ok := methods[*method]; !ok {
+		fmt.Fprintf(os.Stderr, "Error: invalid match method\n")
+		return
+	} else {
+		mm = mmt
 	}
 
 	if *cpuprofile != "" {
@@ -91,6 +109,15 @@ func main() {
 		tolen += olen[n]
 	}
 
+	for i := *fnum - 1; i >= 0; i-- {
+		t := pools[i].Trie().Clone()
+		if trieCat != nil {
+			t.AppendTrie(trieCat)
+		}
+
+		trieCat = t
+	}
+
 	procnum := runtime.NumCPU()
 	ol4proc := *onum / procnum + 1
 	ch := make(chan bool)
@@ -114,9 +141,9 @@ func main() {
 					ol.Append(olg[n])
 				}
 
-				olmap := make([]map[oligo.Oligo]int, *fnum)
+				olmap := make([]map[string]int, *fnum)
 				for n := 0; n < *fnum; n++ {
-					olmap[n] = make(map[oligo.Oligo]int)
+					olmap[n] = make(map[string]int)
 				}
 
 				for d := 0; d < *depth; d++ {
@@ -126,31 +153,13 @@ func main() {
 						fmt.Fprintf(os.Stderr, ".")
 					}
 
-					eolen := eol.Len()
-					start := 0
-					for n := 0; n < *fnum; n++ {
-						eo := eol.Slice(start, start+olen[n])
-						m := pools[n].SearchMin(eo)
-
-						if m != nil {
-							olmap[n][m.Seq]++
-						}
-
-						start += (eolen * olen[n]) / tolen
-						if (eolen * olen[n]) % tolen > tolen/2 {
-							start++
-						}
-
-//						if n+1 < *fnum && start + olen[n+1] > eolen {
-//							start = eolen - olen[n+1]
-//						}
-					}
+					mm(ol, eol, olen, pools, olmap)
 				}
 
 				for n := 0; n < *fnum; n++ {
 					atomic.AddUint64(&avgmatch[n], uint64(len(olmap[n])))
 
-					var eol oligo.Oligo
+					var eol string
 					m := -1
 					for v, i := range olmap[n] {
 						if i > m {
@@ -159,7 +168,7 @@ func main() {
 						}
 					}
 
-					if eol == olg[n] {
+					if eol == olg[n].String() {
 						atomic.AddUint64(&correct[n], 1)
 					}
 
@@ -182,5 +191,99 @@ func main() {
 	fmt.Printf("\n")
 	for n := 0; n < *fnum; n++ {
 		fmt.Printf("Field %d: incorrect %g (%d/%d) matches %v count %v\n", n, float64(total-correct[n])/float64(total), total - correct[n], total, avgcnt[n]/total, avgmatch[n]/total)
+	}
+}
+
+func matchOld(ol, eol oligo.Oligo, olen []int, pools []*utils.Pool, olmap []map[string]int) {
+	eolen := eol.Len()
+	tolen := ol.Len()
+	start := 0
+	for n := 0; n < *fnum; n++ {
+		eo := eol.Slice(start, start+olen[n])
+		m := pools[n].SearchMin(eo)
+
+		if m != nil {
+			olmap[n][m.Seq.String()]++
+		}
+
+		start += (eolen * olen[n]) / tolen
+		if (eolen * olen[n]) % tolen > tolen/2 {
+			start++
+		}
+
+//		if n+1 < *fnum && start + olen[n+1] > eolen {
+//			start = eolen - olen[n+1]
+//		}
+	}
+}
+
+// slight difference with "old", makes it a bit better to fit fields
+func matchSimple(ol, eol oligo.Oligo, olen []int, pools []*utils.Pool, olmap []map[string]int) {
+	eolen := eol.Len()
+	tolen := ol.Len()
+	start := 0
+	stdstart := 0
+	for n := 0; n < *fnum; n++ {
+//		if start+olen[n] > eolen {
+//			start = eolen - olen[n]
+//			if start < 0 {
+//				start = 0
+//			}
+//		}
+
+		eo := eol.Slice(start, start+olen[n])
+		m := pools[n].SearchMin(eo)
+
+		if m != nil {
+			olmap[n][m.Seq.String()]++
+		}
+
+		stdstart += olen[n]
+		start = (eolen * stdstart) / tolen
+	}
+}
+
+// after matching by the trie, use oligo.Find to try to get the end of the match
+func matchFind(ol, eol oligo.Oligo, olen []int, pools []*utils.Pool, olmap []map[string]int) {
+	eolen := eol.Len()
+	tolen := ol.Len()
+	start := 0
+	stdstart := 0
+	for n := 0; n < *fnum; n++ {
+		if start+olen[n] > eolen {
+			start = eolen - olen[n]
+			if start < 0 {
+				start = 0
+			}
+		}
+
+		eo := eol.Slice(start, start+olen[n])
+		m := pools[n].SearchMin(eo)
+
+		if m != nil {
+			olmap[n][m.Seq.String()]++
+		}
+
+		stdstart += olen[n]
+		if m != nil {
+			pos, len := oligo.Find(eol, m.Seq, m.Dist)
+			start = pos + len
+		} else {
+			start = (eolen * stdstart) / tolen
+			if (eolen * olen[n]) % tolen > tolen/2 {
+					start++
+			}
+		}
+	}
+}
+
+// search for the whole sequence in the concatenated trie, then check if any of the fields match
+func matchTriecat(ol, eol oligo.Oligo, olen []int, pools []*utils.Pool, olmap []map[string]int) {
+	m := trieCat.SearchMin(eol)
+	start := 0
+	for n := 0; n < *fnum; n++ {
+		mol := m.Seq.Slice(start, start+olen[n])
+		olmap[n][mol.String()]++
+		start += olen[n]
 	}
 }
